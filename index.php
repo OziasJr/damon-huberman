3f7b882765d341c893275958ba7a5ccf99415926
Ozias Junior <oziasdias29@gmail.com>
2021-05-21 11:00:55 -0300

    php

--- index.php
+++ index.php
@@ -1,25 +1,23 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
-    <link rel="stylesheet" href="./assets/css/style.css">
-    <title>DAMON HUBERMAN</title>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <?php include('./pages/links/links.css.php'); ?>
+  <title>DAMON HUBERMAN</title>
 </head>
+
 <body>
-    <div class="structure">
-      <?php include('./pages/header.php'); ?>
-      <?php include('./pages/main.php'); ?>
-      <?php include('./pages/footer.php'); ?>
-    </div>
+  <div class="structure">
+    <?php include('./pages/header.php'); ?>
+    <?php include('./pages/main.php'); ?>
+    <?php include('./pages/footer.php'); ?>
+  </div>
 
 
-
-
-    
-    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v10.0&appId=284615029380357&autoLogAppEvents=1" nonce="yjSVpQ6O"></script>
-    <script src="./assets/js/script.js"></script>
+  <?php include('./pages/links/links.script.php'); ?>
 </body>
+
 </html>
---
+++ pages/links/links.script.php
@@ -0,0 +1,2 @@
+<script async defer crossorigin="anonymous" src="https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v10.0&appId=284615029380357&autoLogAppEvents=1" nonce="yjSVpQ6O"></script>
+<script src="./assets/js/script.js"></script>
