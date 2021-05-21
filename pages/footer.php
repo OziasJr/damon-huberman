5f2dbfe3bcd8865b27407b9c28fccba4eb081458
Ozias Junior <oziasdias29@gmail.com>
2021-05-21 10:42:52 -0300

    index and php

---
+++ pages/footer.php
@@ -0,0 +1,4 @@
+<footer class="footer text-white align-items-center d-flex justify-content-between">
+    <p class="p-4">© Damon Huberman - 2021 - Todos os direitos reservados</p>
+    <img class="p-2" src="./assets/icon/pawprint.svg" alt="">
+</footer>
---
+++ pages/header.php
@@ -0,0 +1,14 @@
+<header class="header">
+    <h1 class="h1">Damon Huberman</h1>
+    <nav class="nav d-flex align-items-center p-3 justify-content-between">
+        <ul class="d-flex">
+        <li><a href="#" class="btn p-3 text-secondary"><b>Home</b></a></li>
+        <li><a href="#" class="btn p-3 text-white">Sobre</a></li>
+        <li><a href="#" class="btn p-3 text-white">Consultoria de Imagem e Estilo</a></li>
+        <li><a href="#" class="btn p-3 text-white">Contato</a></li>
+        </ul>
+        <form action="" class="col-12 col-lg-auto mb-3 mb-lg-0 me-lg-3">
+            <input type="search" class="form-control form-control-dark" aria-label="Search" placeholder="Search...">
+        </form>
+    </nav>
+</header>
